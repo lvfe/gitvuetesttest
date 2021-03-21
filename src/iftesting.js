dfc8d56772cbbc0e98126113d0b15863f3e75be0
fengjiao <lvfe@tcd.ie>
2021-03-21 19:04:33 +0800

    s

---
+++ src/iftesting.js
@@ -0,0 +1,15 @@
+const onlyforTesting = (args) => {
+    if(args>1){
+        if(args<2){
+            if(args<4){
+                if(args<6){
+                    if(args<8){
+                        console.log('hit it!');
+                        console.log('hit it!');
+                    }
+                }
+            }
+        }
+    }
+}
+module.exprts = onlyforTesting;
